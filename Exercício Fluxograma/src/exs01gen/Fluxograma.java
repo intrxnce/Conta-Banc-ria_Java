536034bb4a9a3166db5d23dd9482b856a6dd36dd
Leticia Oliveira <leticia.oliveira201489@gmail.com>
2023-04-01 16:28:34 -0300

    commit backup

---
+++ Exercício Fluxograma/src/exs01gen/Fluxograma.java
@@ -0,0 +1,25 @@
+package exs01gen;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Fluxograma {
+	
+	public static void main(String[] args) {
+		
+		DecimalFormat df = new DecimalFormat("###,##0.00");
+		
+		Scanner leia = new Scanner (System.in);
+		float salario, abono;
+		
+		System.out.println("Digite o valor do salário: ");
+		salario = leia.nextFloat();
+		
+		System.out.println("Digite o valor do abono: ");
+		abono = leia.nextFloat();
+		
+		System.out.println("Novo salário: R$ " + df.format((salario+abono)));
+		
+	}
+
+}
